d964d23bcb23d5f1e97af1b6b670fbd1bb98881b
Oscar <3311764+oscarfh@users.noreply.github.com>
2020-05-04 18:47:37 -0300

    Configure

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -1,5 +1,6 @@
 package org.acme;
 
+import com.google.common.base.Strings;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,6 +12,9 @@ public class ExampleResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+
+        Strings.isNullOrEmpty("myString");
+
         return "hello";
     }
 }
